54466fbe16851aa2983ce63fb7e08b5f8538deb7
Gabriel Ataide <gabriel.ataide@enforcegroupe.com>
2021-08-20 20:26:28 -0300

    Basic packages, and drive setup

---
+++ install.sh
@@ -0,0 +1,41 @@
+# Setting up the correct timezone
+timedatectl set-ntp true
+
+# Updating package database
+pacman -Sy --noconfirm
+
+# Updating mirrorlist
+echo "Setting up mirrors"
+pacman -S reflector --noconfirm
+reflector --latest 200 --country Brazil,Canada,Japan,Australia,Iceland,Norway --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+#Setting up dependencies
+echo "Installing prerequisites"
+pacman -S --noconfirm btrfs-progs
+
+echo " Select your disk to format"
+
+lsblk
+
+echo "Please enter disk: (example /dev/sda)"
+
+read DISK
+
+echo -e "Formatting disk...$HR"
+
+dd if=/dev/zero of=${DISK} status=progress
+
+sgdisk -a 2048 -o ${DISK}
+
+sgdisk -n 1:0:+550M ${DISK}
+
+sgdisk -n 2:0:0     ${DISK}
+
+sgdisk -t 1:ef00 ${DISK}
+
+sgdisk -t 2:8300 ${DISK}
+
+sgdisk -c 1:"UEFISYS" ${DISK}
+
+sgdisk -c 2:"ROOT" ${DISK}
+
